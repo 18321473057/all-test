97bb5b436b0ba3ad937dc1b96671cea2754f3f6b
18321473057 <ycs19930606>
2019-12-10 07:57:17 +0800

    大

--- allparent/order/src/main/java/org/line/learn/service/impl/OrderService.java
+++ allparent/order/src/main/java/org/line/learn/service/impl/OrderService.java
@@ -1,8 +1,7 @@
-package org.line.learn.controller.service.impl;
+package org.line.learn.service.impl;
 
-
-import org.line.learn.controller.service.IOrderService;
 import org.line.learn.repertory.RepertoryFeign;
+import org.line.learn.service.IOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +10,6 @@ public class OrderService implements IOrderService {
 
     @Autowired
     private RepertoryFeign repertoryFeign;
-
 
     @Override
     public Integer getTotleNumber(String sku) {
